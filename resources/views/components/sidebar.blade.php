0a3a8ced31cbb40d33cdc51de0fbf8ac65c4963c
mohagungnursalim <mohagungnursalim@gmail.com>
2025-02-03 00:41:15 +0800

    refactor: mematikan a href saat route diakses

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,21 +5,32 @@ class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -transla
 aria-label="Sidebar">
 <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
     <ul class="space-y-2 font-medium">
+        
+
         <li>
-            <a wire:navigate href="/dashboard"
-                class="flex items-center p-2 rounded-lg {{ Request::is('dashboard') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }} group">
+            <a 
+                class="flex items-center p-2 rounded-lg {{ Request::is('dashboard') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }}"
+                @if (!Request::is('dashboard'))
+                    wire:navigate href="/dashboard"
+                @endif>
                 <i class="bi bi-speedometer"></i>
                 <span class="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
             </a>
         </li>
-
+        
         <li>
-            <a wire:navigate href="/dashboard/transactions"
-                class="flex items-center p-2 rounded-lg {{ Request::is('dashboard/transactions') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }} group">
+            <a 
+                class="flex items-center p-2 rounded-lg {{ Request::is('dashboard/transactions') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }}"
+                @if (!Request::is('dashboard/transactions'))
+                    wire:navigate href="/dashboard/transactions"
+                @endif>
                 <i class="bi bi-bag-check-fill"></i>
                 <span class="flex-1 ms-3 whitespace-nowrap">Transaksi</span>
             </a>
         </li>
+        
+        
+        
 
         <li>
             <button 
@@ -35,18 +46,34 @@ aria-label="Sidebar">
             <ul 
                 id="dropdown" 
                 class="{{ Request::is('dashboard/products') || Request::is('dashboard/suppliers') ? '' : 'hidden' }} py-2 space-y-2">
-                <a wire:navigate href="/dashboard/products"
-                    class="flex items-center p-2 rounded-lg {{ Request::is('dashboard/products') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }} group">
+                
+                <a 
+                    @if (Request::is('dashboard/products'))
+                        class="flex items-center p-2 rounded-lg bg-purple-300 text-dark"
+                    @else
+                        wire:navigate href="/dashboard/products" 
+                        class="flex items-center p-2 rounded-lg hover:bg-gray-200"
+                    @endif
+                >
                     <i class="bi bi-arrow-return-right"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Master Produk</span>
                 </a>
-                <a wire:navigate href="/dashboard/suppliers"
-                    class="flex items-center p-2 rounded-lg {{ Request::is('dashboard/suppliers') ? 'bg-purple-300 text-dark' : 'hover:bg-gray-200' }} group">
+        
+                <a 
+                    @if (Request::is('dashboard/suppliers'))
+                        class="flex items-center p-2 rounded-lg bg-purple-300 text-dark"
+                    @else
+                        wire:navigate href="/dashboard/suppliers" 
+                        class="flex items-center p-2 rounded-lg hover:bg-gray-200"
+                    @endif
+                >
                     <i class="bi bi-arrow-return-right"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Master Supplier</span>
                 </a>
+        
             </ul>
         </li>
+        
         
 
 
