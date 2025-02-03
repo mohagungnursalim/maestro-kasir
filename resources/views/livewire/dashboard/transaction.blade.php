a6a4298641b9e00b2a7021244237c626614e537a
mohagungnursalim <mohagungnursalim@gmail.com>
2025-02-03 14:19:32 +0800

    fix: menghilangkan button min (-) pada cart produk

--- resources/views/livewire/dashboard/transaction.blade.php
+++ resources/views/livewire/dashboard/transaction.blade.php
@@ -54,11 +54,13 @@
                                         </div>
                                         <div class="flex items-center justify-between md:order-3 md:justify-end">
                                             <div class="flex items-center">
-                                                <button {{ $item['quantity'] == 1 ? 'disabled' : ''}} wire:click="updateQuantity({{ $index }}, {{ $item['quantity'] - 1 }})" type="button" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                                                    <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"></path>
-                                                    </svg>
-                                                </button>
+                                                @if ($item['quantity'] > 1)
+                                                    <button wire:click="updateQuantity({{ $index }}, {{ $item['quantity'] - 1 }})" type="button" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-0 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                                                        <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 9h16"></path>
+                                                        </svg>
+                                                    </button>
+                                                @endif
                                                 <input disabled type="text" wire:model="cart.{{ $index }}.quantity" class="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white" value="{{ $item['quantity'] }}">
                                                 <button wire:click="updateQuantity({{ $index }}, {{ $item['quantity'] + 1 }})" type="button" class="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
                                                     <svg class="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
