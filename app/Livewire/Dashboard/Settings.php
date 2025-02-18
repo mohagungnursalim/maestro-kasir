dc9bb0dc43efbe9e4aba1c67f38565b6bb57dcc5
mohagungnursalim <mohagungnursalim@gmail.com>
2025-02-18 13:19:53 +0800

    fix: bug image store logo

--- app/Livewire/Dashboard/Settings.php
+++ app/Livewire/Dashboard/Settings.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Dashboard;
 
 use App\Models\StoreSetting;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -37,27 +38,33 @@ class Settings extends Component
             'store_footer' => 'nullable|string|max:100',
             'new_logo' => 'nullable|image|max:2048',
         ]);
-
+    
         $settings = StoreSetting::firstOrNew([]);
-
+    
         // Handle logo upload
         if ($this->new_logo) {
+            // Simpan gambar baru
             $imagePath = $this->new_logo->store('logos', 'public');
-
-            if (!empty($settings->store_logo) && file_exists(public_path('storage/' . $settings->store_logo))) {
-                @unlink(public_path('storage/' . $settings->store_logo));
+    
+            // Hapus gambar lama jika ada
+            if (!empty($settings->store_logo)) {
+                $oldLogoPath = str_replace('storage/', '', $settings->store_logo);
+                if (Storage::disk('public')->exists($oldLogoPath)) {
+                    Storage::disk('public')->delete($oldLogoPath);
+                }
             }
-
+    
+            // Simpan path baru
             $settings->store_logo = "storage/{$imagePath}";
         }
-
+    
         // Update store settings
         $settings->store_name = $this->store_name ?? 'Default Name';
         $settings->store_address = $this->store_address ?? 'Default Address';
         $settings->store_phone = $this->store_phone ?? '085756000000';
         $settings->store_footer = $this->store_footer ?? 'Default Footer';
         $settings->save();
-
+    
         session()->flash('success', 'Pengaturan berhasil diperbarui!');
         return $this->redirect('/dashboard/store-settings', navigate: true);
     }
