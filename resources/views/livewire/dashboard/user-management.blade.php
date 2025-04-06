f6d06c27982c13043e95df383b536302c1bf896c
mohagungnursalim <mohagungnursalim@gmail.com>
2025-04-06 10:58:52 +0800

    fix: mengganti tampilan manajemen user

--- resources/views/livewire/dashboard/user-management.blade.php
+++ resources/views/livewire/dashboard/user-management.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
+<div>
     <h1 class="text-2xl font-semibold mb-4 text-gray-700">Manajemen User</h1>
 
     <!-- Form Input User -->
