df92c49745c33c89d2072d4ecca250083558c800
mohagungnursalim <mohagungnursalim@gmail.com>
2026-02-26 00:06:45 +0800

    add middleware proxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustProxies
+{
+    protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO;
+}
